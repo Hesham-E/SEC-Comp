84bf82b13874c3fc2a70b633eb73253403813ab0
Etienne Lagace <63128108+EtienneL321@users.noreply.github.com>
2020-11-07 10:55:38 -0700

    added to profile

---
+++ Main files/Profile.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <DateofBirth.h>
+#include <Friends.h>
+#include <Post.h>
+#include <Interest.h>
+
+using namespace std;
+
+#ifndef PROFILE
+#define PROFILE
+
+
+
+
+#endif
