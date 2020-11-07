511ca7db62138e1964c3f4323ff4f0f6a2eba6d1
Rahat Islam <islamrahat735@gmail.com>
2020-11-07 11:40:04 -0700

    added stters and getters to User.h

--- Main files/User.h
+++ Main files/User.h
@@ -5,11 +5,21 @@ using namespace std;
 
 class User {
     string email;
-    string passowrd;
+    string password;
     string userName;
     // Profile Profile; 
     bool isLoggendIn;
     public: 
     User();
+    User(string email, string password, string userName );
+    
+    const string& getEmail() const;
+    const string& getPassword() const;
+    const string& getUserName () const;
+
+    void setEmail(string email);
+    void getPassword(string password);
+    void getUserName(string username);
+
     void changeProfile();
 };
